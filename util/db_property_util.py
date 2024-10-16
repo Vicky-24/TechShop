064d0d6d019ba15a990571a7db1519dc82b583a4
Vikas Reddy Gorantla <114513993+Vicky-24@users.noreply.github.com>
2024-10-16 10:54:01 +0530

    Add files via upload

---
+++ util/db_property_util.py
@@ -0,0 +1,4 @@
+class DBPropertyUtil:
+    @staticmethod
+    def get_connection_string():
+        return 'Driver={SQL Server};Server=LAPTOP-R0NMQQC9\\SQLEXPRESS;Database=TechShopDB;Trusted_Connection=yes;'
